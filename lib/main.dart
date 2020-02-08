c737f89df282706bb059c6e842e409fd49f5c623
Yoshihisa Okada <yoshi19920807@gmail.com>
2020-02-08 10:13:38 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,13 @@
-class Person {
-  String name = 'Max';
-  int age = 30;
-}
-
-double addNumbers(int num1, double num2) {
-  // print(num1 + num2);
-  return num1 + num2;
-}
+import 'package:flutter/material.dart';
 
 void main () {
-  var p1 = Person();
-  print(p1.name);
-  var p2 = Person();
-  p2.name = 'Menu';
-  print(p2.name);
-  double firstResult;
-  //..
-  firstResult = addNumbers(1, 2);
-  
-  /*
-  ...
-  ...
-  */
 
-  print (firstResult + 1);
-  print('Hello!');
+}
+
+
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello!'), );
+  }
 }
