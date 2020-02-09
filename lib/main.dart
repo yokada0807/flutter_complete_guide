3ed29c1af8502fdf63383b8de004aae2dd740832
yoshihisa okada <yookada@winoo.co.jp>
2020-02-09 23:37:00 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main () {
+// void main () {
+//  runApp(MyApp());
+
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
 
 }
 
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
 
-class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'), );
+    var questions = [
+      'what\'s your favorite animal?',
+      'what\'s your favorite color?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 was chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('answer 3 is chosen');
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
